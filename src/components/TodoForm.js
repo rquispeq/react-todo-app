fb834b2d98070348764c605b81614375c1979867
Rony Quispe <oghmand@gmail.com>
2021-08-22 19:53:37 -0500

    Todo list app

---
+++ src/components/TodoForm.js
@@ -0,0 +1,43 @@
+import { useState,useEffect,useRef } from "react"
+
+const TodoForm = (props) => {
+    const [input, setInput] = useState(props.edit ? props.edit.value : '')
+
+    const inputRef = useRef(null)
+
+    useEffect( () => {
+        inputRef.current.focus()
+    })
+
+    const handleChange = e => {
+        setInput(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: input
+        })
+
+        setInput('')
+    }
+
+    return (
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder= {props.edit ? 'Edit a todo' : 'Add a todo'}
+                value={input}
+                name="text"
+                className="todo-input"
+                onChange={handleChange}
+                ref={inputRef}
+            />
+            <button className="todo-button">{ props.edit ? 'Edit' : 'Add' } todo</button>
+        </form>
+    )
+}
+
+export default TodoForm
